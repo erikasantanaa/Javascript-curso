156105a4c45c7e4ea6e5c0f0d123010ca5844a50
Erika Santana <erika.araujo12344@gmail.com>
2021-01-11 18:59:07 -0300

    continuando os estudos

---
+++ cursoJS/const_01.js
@@ -0,0 +1,7 @@
+const myBirthYear = 1992;
+const currentYear = 2021;
+
+const myAge = currentYear - myBirthYear;
+const ten = 10;
+
+console.log(`Em ${currentYear + ten}, estarei com ${myAge + ten} anos.`)
