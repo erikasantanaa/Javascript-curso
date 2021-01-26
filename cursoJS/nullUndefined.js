6ee7af37c9775d2508018150c18b2bf80f2253df
Erika Santana <erika.araujo12344@gmail.com>
2021-01-25 23:03:50 -0300

    exercicio js

---
+++ cursoJS/nullUndefined.js
@@ -0,0 +1,9 @@
+//Null
+let emptiness = null
+console.log(emptiness, emptiness + 3, `O valor é ${emptiness}`)
+
+console.log('--------------------------')
+
+//Undefined
+let UndefinedExep 
+console.log(UndefinedExep, UndefinedExep + 3, `O valor é ${UndefinedExep}`)
