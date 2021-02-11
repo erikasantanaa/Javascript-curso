369eade36c0a9276020efe674e99d9dcdd9bf12c
Erika Santana <erika.araujo12344@gmail.com>
2021-02-11 11:57:27 -0300

    exercicio 5 concluido

--- exerciciosCursoJS/exerc05/loop.js
+++ exerciciosCursoJS/exerc05/loop.js
@@ -20,21 +20,21 @@
     5
 */
 
-// let i = 0
+let i = 0
 
-// while (i < 6){
-//   console.log(`${i}`)
-//   i++
-// }
+while (i <= 5){
+  // console.log(i)
+  i++
+}
 
 /*
   02 - Comente a declaração da let e o loop acima e:
   - Utilizando um for loop, faça o mesmo que foi pedido no exercício 01.
 */
 
-// for (let i = 0; i < 6; i++){
-//   console.log(`${i}`)
-// }
+for (let i = 0; i < 6; i++){
+  // console.log(i)
+}
 
 
 /*
@@ -47,11 +47,13 @@
   "Esta é a Xª vez que esta frase é exibida no console."
 */
 
+for (let i = 1; i <= 10; i++ ){
+  // console.log(`Esta é a ${i} vez que esta frase é exibida no console.`)
+}
 
 
 /*
   04 - Comente o loop acima e:
-
   - Abaixo da constante "cities", declare uma let "upperCaseCities" e atribua à  
     ela um array vazio;
   - Adicione no array que a "upperCaseCities" armazena as cidades do array  
@@ -60,10 +62,16 @@
 */
 
 const cities = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais']
+let upperCaseCities = []
+
+for (let i = 0; i < cities.length; i++){
+  upperCaseCities.push(cities[i].toUpperCase())
+}
+// console.log(upperCaseCities)
+
 
 /*
   05 - Comente o console.log acima e:
-
   - Após a const names, utilize um for loop para gerar um template HTML;
   - Exiba no console o template HTML que você gerou;
   - O template deve ser exibido apenas uma vez no console e o resultado deve ser:
@@ -72,9 +80,15 @@ const cities = ['São Paulo', 'Rio de Janeiro', 'Minas Gerais']
 
 const names = ['João Grilo', 'Chicó', 'Rosinha']
 
+let template = ''
+
+for (let i = 0; i <= names.length; i++){
+   template += `<li>${names[i]}</li>`
+  }
+  // console.log(template)
+  
 /*
   06 - Comente o console.log acima e:
-
   - Após a const numbers, utilize um for loop para somar todos os números do  
     array;
   - Exiba no console um boolean indicando se o total dessa soma é 337;
@@ -83,13 +97,18 @@ const names = ['João Grilo', 'Chicó', 'Rosinha']
 
 const numbers = ['91', '5', '30', '97', '83', '31']
 
+let sun = 0
+
+for (let i = 0; i < numbers.length; i++){
+  sun += Number(numbers[i])
+}
+// console.log(sun === 337)
+
 /*
   07 - Comente o console.log acima.
-
   Este exercício é um pouco mais complexo que os anteriores. Não se preocupe  
   muito caso você não acerte ele. Apenas tente resolvê-lo com o conhecimento  
   adquirido até aqui.
-
   - O JavaScript permite você criar arrays dentro de um outro array. Isso é  
     chamado de aninhamento de arrays, ou nested arrays em inglês. Por isso, o  
     array "arrays" abaixo é uma sintaxe válida. Cada array dentro do array  
@@ -99,6 +118,8 @@ const numbers = ['91', '5', '30', '97', '83', '31']
   - Após o for, exiba no console o resultado, que deve ser 218.
 
   Dica: você pode colocar um loop dentro do outro, ou seja, aninhá-los. =)
+
+  anhimaneto de array:
 */
 
 const arrays = [
@@ -107,3 +128,13 @@ const arrays = [
   [8, 32, 16],
   [2, 8, 4]
 ]
+
+let arraysSum = 0
+
+for (let i = 0; i < arrays.length; i++){
+  for (let j = 0; j < arrays[i].length; j++){
+    arraysSum += arrays[i][j]
+  }
+}
+
+console.log(arraysSum)// resultado 218
