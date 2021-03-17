fdf6c2194dfc64e8b01798155653fef992f8ede9
Erika Santana <erika.araujo12344@gmail.com>
2021-03-17 16:53:04 -0300

    js

---
+++ curso-js/arrowFunctions.js
@@ -0,0 +1,14 @@
+//Função Tradicional
+const double = function (number){
+    return number * 2
+}
+
+//Arrow function
+
+
+
+
+
+
+const result = double(3)
+console.log(`O resultado é: ${result}`)
--- curso/funcao.js
+++
@@ -1,9 +0,0 @@
-//SOMA DE DOIS NUMEROS:
-
-function soma(n1, n2){
-    let resultado = n1 + n2;
-    return resultado;
-}
-
-let resultado = soma(1 , 3);
-console.log(resultado);
