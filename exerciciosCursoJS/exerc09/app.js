d26a24820d578fbad95748f8389b33cdde332981
Erika Santana <erika.araujo12344@gmail.com>
2021-03-24 15:45:05 -0300

    exercicio 9

--- exerciciosCursoJS/exerc09/app.js
+++ exerciciosCursoJS/exerc09/app.js
@@ -1,112 +1,104 @@
-/*  Dica:
-  - Alguns exercícios utilizarão métodos, propriedades e funcionalidades vistos  
+/* Dica: - Alguns exercícios utilizarão métodos, propriedades e funcionalidades vistos  
     no começo do curso. Se for necessário, reveja partes das aulas anteriores  
-    para relembrar.
-*/
-/*01 - Converta a função abaixo em uma arrow function e utilize-a para exibir um  
-    valor no console.
-*/
+    para relembrar.*/
+/*01 - Converta a função abaixo em uma arrow function e utilize-a para exibir um valor no console.
+
+typeof: para ver o tipo */
 
 // function convertToString (value) {
 //     return String(value)
 //   }
 
-  const convertToString = value => String(value)
+  const convertToString = value => String(value);
 
-  console.log(convertToString(true))
+  console.log(typeof convertToString(true));
   
-  /*
-    02
+  /* 02 - Crie uma função que retorne a quantidade de caracteres que uma string  
+      recebida por parâmetro possui.  */
+
+  const retornaQuantidade = string => string.length;
+
+  console.log(retornaQuantidade('erika'));
   
-    - Crie uma função que retorne a quantidade de caracteres que uma string  
-      recebida por parâmetro possui.
-  */
-  
-  /*
-    03
-  
-    - Crie uma função que retorne todos os caracteres de uma string em letras  
+  /* 03- Crie uma função que retorne todos os caracteres de uma string em letras  
       minúsculas;  
     - Utilize a função para exibir a string abaixo no console.
   
-    "CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO"
-  */
+    "CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO" */
+
+  const converterMinusculo = string => string.toLowerCase();
+
+  console.log(converterMinusculo('CHOCOTONE E OVO DE PÁSCOA JUNTOS NO MERCADO EM PLENO FEVEREIRO'));
+
   
-  /*
-    04
+  /* 04- Crie uma função que recebe 2 parâmetros: um caractere e uma string;
+    - Ao ser invocada, a função deve retornar o index do caractere na string. */
+
+  const getIndex = (caracter, string) => string.indexOf(caracter);
+
+  console.log(getIndex('2', '123'));
+
   
-    - Crie uma função que recebe 2 parâmetros: um caractere e uma string;
-    - Ao ser invocada, a função deve retornar o index do caractere na string.
-  */
+  /*05- Crie uma função que, ao ser invocada, retorna um boolean indicando se o item passado por argumento existe no array (também passado por argumento).*/
+
+  const retornaBoolean = (item, array) => array.includes(item);
   
-  /*
-    05
+  console.log(retornaBoolean(7, [1, 2, 3]))
   
-    - Crie uma função que, ao ser invocada, retorna um boolean indicando se o item  
-      passado por argumento existe no array (também passado por argumento).
-  */
+  /*06- Crie uma função que retorna a concatenação de 2 arrays, passados como  
+      argumentos em sua invocação;*/
+
+  const concatenaArrays = (array1, array2)  => array1.concat(array2)
+
+  console.log(concatenaArrays('123', '456'))
   
-  /*
-    06
   
-    - Crie uma função que retorna a concatenação de 2 arrays, passados como  
-      argumentos em sua invocação;
-  */
+  /* 07- Crie uma função que retorna o array passado como argumento em sua invocação, mas com o último item removido.*/
   
-  /*
-    07
+  const removeItem = array => {
+    array.pop()
+    return array
+  }
+  console.log(removeItem(['Erika', 'Maria', 'Santana']))
+
   
-    - Crie uma função que retorna o array passado como argumento em sua invocação,  
-      mas com o último item removido.
-  */
+  /*08 - Crie uma função que retorna se o valor passado como argumento em sua invocação é null. */
+
+  const isNull = value => value === null
+
+  console.log(isNull(null))
   
-  /*
-    08
-  
-    - Crie uma função que retorna se o valor passado como argumento em sua  
-      invocação é null.
-  */
-  
-  /*
-    09
-  
-    - Crie uma função que apenas invoca uma função de callback recebida por  
-      parâmetro;
+  /* 09 - Crie uma função que apenas invoca uma função de callback recebida por parâmetro;
     - Crie outra função que apenas exibe seu nome no console;
     - Invoque a função que recebe um callback por parâmetro, passando como  
       argumento a função que exibe seu nome no console e veja se o nome realmente  
-      foi exibido.
-  */
+      foi exibido. */
+
+  const invocaCallback = callback => callback()
+
+  const nome = () => console.log('Erika Santana')
   
-  /*
-    10
+  invocaCallback(nome)
   
-    - Crie uma função que invoca uma função de callback recebida por parâmetro.  
+  /* 10 - Crie uma função que invoca uma função de callback recebida por parâmetro.  
       A invocação da função recebida por parâmetro deve receber um valor como  
       argumento;
     - Crie uma função que retorna o triplo de um número recebido por parâmetro;
     - Faça com que a invocação da função descrita no 1º item deste exercício (10)  
-      resulte no triplo de 33.
-  */
-  
-  /*
-    11
-  
-    - Utilizando um forEach, baseado no array "numbers", a cada iteração, exiba a  
-      mensagem abaixo no console, substituindo os "X" pelas informações corretas;
+      resulte no triplo de 33. */
+
     
-    "O Xº item do array [X, X, X] é X."
-  */
+  
+  /* 11 - Utilizando um forEach, baseado no array "numbers", a cada iteração, exiba a  
+      mensagem abaixo no console, substituindo os "X" pelas informações corretas;
+
+    "O Xº item do array [X, X, X] é X."*/
   
   // const numbers = [1, 2, 3]
   
-  /*
-    12
-  
-    - Converta o for loop abaixo em um forEach;
+  /* 12- Converta o for loop abaixo em um forEach;
     - Após a conversão, verifique se a cópia do array lettersCopy realmente foi  
-      criada.
-  */
+      criada.*/
   
   // const letters = ['v', 'e', 'p']
   // let lettersCopy = []
