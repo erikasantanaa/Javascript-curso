a91fc34d50cd77dcf9a9b1f21c71d750725a0fd7
Erika Santana <erika.araujo12344@gmail.com>
2021-04-14 15:48:52 -0300

    objetos

---
+++ curso-js/objetosEmArray.js
@@ -0,0 +1,16 @@
+//usado para guardar informações seja de um banco de dados ou API.
+
+const posts = [
+    { title: 'Coxinha', likes: 30},
+    { title: 'Brigadeiro', likes: 25}
+]
+
+//outro exemplo
+
+let user = {
+    blogPosts: [
+        { title: 'Coxinha', likes: 30},
+        { title: 'Brigadeiro', likes: 25}
+    ]
+}
+
