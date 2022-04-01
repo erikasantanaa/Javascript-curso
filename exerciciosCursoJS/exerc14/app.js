1b7ff4b41ff8fbe5e83bed853951c282e967f382
Erika Santana <erikamariasantanaa@gmail.com>
2022-03-31 22:18:48 -0300

    exercicio concluído

--- exerciciosCursoJS/exerc14/app.js
+++ exerciciosCursoJS/exerc14/app.js
@@ -3,11 +3,11 @@
   com todas as letras maiúsculas.
 */
 
-const titulo = document.querySelector('h1')
+const title = document.querySelector('h1')
 
-console.log(titulo)
+console.log(title)
 
-titulo.textContent = titulo.textContent.toUpperCase()
+title.textContent = title.textContent.toUpperCase()
 
 
 /*02
@@ -25,17 +25,35 @@ const insertNumberIntoUl = number => {
   ul.innerHTML += `<li class="number">${number}</li>`
 }
 
-numbers.forEach(insertNumberIntoUl)
+numbers.forEach(insertNumberIntoUl) // puxando o array para aparcer na tela
 
 /*
+// console.log(li.textContent) // puxando array para aparecer no console  
   03
 
   - Modifique as cores dos números dentro da ul da seguinte forma:
     - Se o número é par, ele deve ser exibido na cor "lightblue";
     - Se o número é ímpar, exiba-o na cor "pink".
+
+    -> quebrar as tarefas:
+    1- exibir os números de cada li no console;
+    2- * se o resto do n° dividido por 2 é 0, ele é par. (exibir so os n° pares)
+    3- * 
 */
 
 const lis = document.querySelectorAll('.number')
+
+const changeLiColor = lis.forEach(li => {
+  const isEven = Number(li.textContent) % 2 === 0  //convertendo a string p/ number 
+
+
+  if (isEven) {
+    li.style.color = 'lightblue'
+    return
+  }
+
+  li.style.color = 'pink'
+})
 
 
 
@@ -45,7 +63,18 @@ const lis = document.querySelectorAll('.number')
   - Adicione a classe "body-background" no corpo do index.html;
 
   P.s: a classe "body-background" já está declarada no style.css.
+
+  obs: duas formas para inserir body:
+  1- const body = document.querySelector('body')
+
+  2- const body = document.body
 */
+
+const body = document.querySelector('body')
+
+// console.log(body)
+
+body.classList.add('body-background')
 
 
 
@@ -57,7 +86,9 @@ const lis = document.querySelectorAll('.number')
     do link do index.html.
 */
 
+const repoLink = document.querySelector('a')
 
+repoLink.setAttribute('href', 'https://github.com/roger-melo-treinamentos/curso-de-js-roger-melo')
 
 /*
   06
@@ -65,15 +96,14 @@ const lis = document.querySelectorAll('.number')
   - Exiba o novo valor do atributo href do link no console.
 */
 
-
-
+console.log(repoLink.getAttribute('href'))
 /*
   07
 
-  - Exiba, no console, um objeto com todas as propriedades CSS que podem ser  
-    manipuladas via JS no h1.
+  - Exiba, no console, um objeto com todas as propriedades CSS que podem 
+  ser manipuladas via JS no h1.
 */
-
+console.log(title.style)
 
 
 /*
@@ -82,7 +112,7 @@ const lis = document.querySelectorAll('.number')
   - Remova a classe "body-background", do elemento body.
 */
 
-
+body.classList.remove('body-background')
 
 /*
   09
@@ -90,3 +120,6 @@ const lis = document.querySelectorAll('.number')
   - Se o link da página possuir uma classe "link", remova-a;
   - Não utilize o método remove() para fazer isso.
 */
+
+repoLink.classList.toggle('link')
+repoLink.classList.toggle('link')
