82a0106c74345a02c5f6491b9fe45d13e1e5e924
Erika Santana <erikamariasantanaa@gmail.com>
2022-05-04 22:48:22 -0300

    Curso em andamento

---
+++ curso-js/eventosDeClique/index.js
@@ -0,0 +1,15 @@
+//inserir eventos
+const button = document.querySelector('button')
+
+button.addEventListener('click', () => {
+    // console.log('clicou aqui')
+})
+
+const lis = document.querySelectorAll('li')
+lis.forEach(li => {
+    li.addEventListener('click', event => {
+        const clickedElement = event.target
+
+        clickedElement.style.textDecoration = 'line-through'
+    })
+})
