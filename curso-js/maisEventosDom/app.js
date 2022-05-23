adb3d9591d823532f8ce8f90571eef94159fe799
Erika Santana <erikamariasantanaa@gmail.com>
2022-05-23 19:11:18 -0300

    estudos manipulação DOM/Eventos

---
+++ curso-js/maisEventosDom/app.js
@@ -0,0 +1,18 @@
+const paragraph = document.querySelector('.copy-me')
+
+paragraph.addEventListener('copy', () => {
+    console.log('texto copiado')
+
+})
+
+const divBox = document.querySelector('.box')
+
+divBox.addEventListener('mousemove', event => {
+    divBox.textContent = `X ${event.offsetX} | Y ${event.offsetY}` 
+
+})
+
+document.addEventListener('wheel', event => {
+    console.log(event.pageX, event.pageY)
+    
+})
