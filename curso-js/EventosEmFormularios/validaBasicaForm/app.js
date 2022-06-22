06c1d3e125e83050f09b931ec8158dcb5f421cf8
erikasantanaa <erikamariasantanaa@gmail.com>
2022-06-22 15:55:28 -0300

    estudando

--- curso-js/EventosEmFormularios/validaBasicaForm/app.js
+++ curso-js/EventosEmFormularios/validaBasicaForm/app.js
@@ -2,12 +2,12 @@
 const form = document.querySelector('.signup-form')
 const feedback = document.querySelector('.feedback')
 
+const testUsername = username => /^[a-zA-Z]{6,12}$/.test(username)
+
 form.addEventListener('submit', event => {
     event.preventDefault()
-
-    const username = event.target.username.value
-    const usernameRegex = /^[a-zA-Z]{6,12}$/
-    const isAvalidUsername = username.test(username)
+    
+    const isAvalidUsername = testUsername(event.target.username.value)
 
     if (isAvalidUsername) {
         feedback.textContent = 'username válido =)'
@@ -15,4 +15,16 @@ form.addEventListener('submit', event => {
     }
     
     feedback.textContent = 'o username deve conter entre 6 a 12 caracteres e apenas letras'
+})
+
+//validar em tempo real
+form.username.addEventListener('keyup', event => {
+    const isAvalidUsername = testUsername(event.target.value)
+
+    if (isAvalidUsername) {
+        form.username.setAttribute('class', 'success')
+        return
+    }
+
+    form.username.setAttribute('class', 'error')
 })
--- exerciciosCursoJS/exerc18/app.js
+++ exerciciosCursoJS/exerc18/app.js
@@ -1,41 +1,51 @@
-/*
-  Apenas 3 exercícios, mas que exigem um certo nível de conhecimento do que  
-  vimos até aqui =)
-*/
+/*Apenas 3 exercícios, mas que exigem um certo nível de conhecimento do que  
+  vimos até aqui =)*/
 
-/*
-  01
-
+/*01
   - Valide o valor do input "username" à medida em que ele é digitado;
   - Ele deve conter: 
-    - No mínimo 6 caracteres;
-    - Apenas letras maiúsculas e/ou minúsculas;
+    - No mínimo 6 caracteres; ok
+    - Apenas letras maiúsculas e/ou minúsculas; ok
   - Se o valor inserido não é válido, exiba um parágrafo laranja abaixo do  
     input com a seguinte mensagem: "O valor deve conter no mínimo 6 caracteres,  
-    com apenas letras maiúsculas e/ou minúsculas";
+    com apenas letras maiúsculas e/ou minúsculas"; ok
   - Se o valor é válido, o parágrafo deve ser verde e exibir a mensagem  
-    "Username válido =)";
+    "Username válido =)";pk
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
   - Não insira o parágrafo manualmente no index.html.
   
-  Dica: pesquise pelo método "insertAdjacentElement", no MDN;
-*/
+  Dica: pesquise pelo método "insertAdjacentElement", no MDN;*/
 
-/*
-  02
+const inputUsername = document.querySelector('#username')
 
+inputUsername.addEventListener('keyup', event => {
+  const inputValue = event.target.value
+  const usernameRegex = /^[a-zA-Z]{6,}$/
+
+  const p = document.createElement('p')
+
+
+  /*o not ( ! ) vai inverter o booblean, no qual a invocação resultou.*/
+  if (!usernameRegex.test(inputValue)) {
+    p.textContent = 'O valor deve conter no mínimo 6 caracteres, com apenas letras maiúsculas e/ou minúsculas'
+    return
+  }
+  p.textContent = 'username valido =)'
+})
+
+
+
+
+/*  02
   - Valide o envio do form;
   - Se o username inserido no input é válido, no envio do form, exiba um  
     parágrafo verde abaixo do botão com a mensagem "Dados enviados =)";
   - Se no momento do envio, o valor do input é inválido, o parágrafo deve ser  
     vermelho e exibir "Por favor, insira um username válido".
   - Use as classes disponíveis no arquivo style.css para colorir o parágrafo;
-  - Não insira o parágrafo manualmente no index.html.
-*/
+  - Não insira o parágrafo manualmente no index.html.*/
 
-/*
-  03
-
+/*  03
   - Há algumas aulas, falamos sobre o método some;
   - Neste exercício, seu desafio será criar este método do zero;
   - Implemente uma função "some" que possui a mesma funcionalidade do método  
@@ -51,5 +61,4 @@
     2) Pesquisar no MDN.
   
   Spoiler alert: este tipo de exercício será frequente em etapas mais avançadas  
-  do curso, onde falaremos sobre TDD. Vá se aquecendo =D
-*/
+  do curso, onde falaremos sobre TDD. Vá se aquecendo =D */
