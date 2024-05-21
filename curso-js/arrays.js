43a155ddaa56eb4cfe6b667fccd5ed0926291d9c
erikasantanaa <erikamariasantanaa@gmail.com>
2024-05-20 23:47:19 -0300

    comentando uma linha

--- curso-js/arrays.js
+++ curso-js/arrays.js
@@ -6,6 +6,6 @@ const joinHeroes = heroes.join('-')
 const indexOf25 = ages.indexOf('25')
 const moreHeroes = heroes.concat(['Superman', 'Wolverine'])
 const pushToHeroes = heroes.push('Cyclops', 'Hulk')
-const popHeroes = heroes.pop()
+// const popHeroes = heroes.pop()
 
 console.log(moreHeroes)
